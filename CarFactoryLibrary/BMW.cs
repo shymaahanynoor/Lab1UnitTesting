33c209863fed10c509fbd9c3000baca134916efe
shymaahanynoor <139652535+shymaahanynoor@users.noreply.github.com>
2024-05-14 20:33:59 +0200

    Add project files.

---
+++ CarFactoryLibrary/BMW.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactoryLibrary
+{
+    public class BMW : Car
+    {
+        public override void Accelerate()
+        {
+            velocity += 15;
+            driven_distance += 30;
+        }
+    }
+}
---
+++ CarFactoryLibrary/CarTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactoryLibrary
+{
+    public enum CarTypes
+    {
+        BMW,
+        Toyota,
+        Audi,
+        Honda,
+        Suzuki
+    }
+}
